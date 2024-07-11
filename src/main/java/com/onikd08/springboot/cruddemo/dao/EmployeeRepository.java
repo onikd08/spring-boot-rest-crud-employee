04cb8222d5b7dd4b223ae250e11dc66ff8763439
onikd08 <onikd08@gmail.com>
2024-07-11 07:49:30 +0300

    Play with spring-data-rest configs

--- src/main/java/com/onikd08/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/onikd08/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.onikd08.springboot.cruddemo.dao;
 
 import com.onikd08.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+// changing path
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
